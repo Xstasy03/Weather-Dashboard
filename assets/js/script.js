abe163e78b6aecd95442f9fad5da9f780ca0b0c7
Xstasy03 <95896830+Xstasy03@users.noreply.github.com>
2022-02-04 20:58:33 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+var owmAPI = "788d5638d7c8e354a162d6c9747d1bdf";
+var currentCity = "";
+var lastCity = "";
+
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
 
