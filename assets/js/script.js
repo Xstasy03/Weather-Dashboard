496e9bc301b50cbf054c23e6d5e85cb007763c04
Xstasy03 <95896830+Xstasy03@users.noreply.github.com>
2022-02-04 22:11:19 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,25 +11,25 @@ var handleErrors = (response) => {
 
 var getCurrentConditions = (event) => {
     let city = $('#search-city').val();
-    currentCity= $('#search-city').val();
+    currentCity = $('#search-city').val();
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
     fetch(queryURL)
-    .then(handleErrors)
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        saveCity(city);
-        $('#search-error').text("");
-        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-        let currentTimeUTC = response.dt;
-        let currentTimeZoneOffset = response.timezone;
-        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
-        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
-        renderCities();
-        getFiveDayForecast(event);
-        $('#header-text').text(response.name);
-        let currentWeatherHTML = `
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            saveCity(city);
+            $('#search-error').text("");
+            let currentWeatherIcon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            let currentTimeUTC = response.dt;
+            let currentTimeZoneOffset = response.timezone;
+            let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+            let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+            renderCities();
+            getFiveDayForecast(event);
+            $('#header-text').text(response.name);
+            let currentWeatherHTML = `
             <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
             <ul class="list-unstyled">
                 <li>Temperature: ${response.main.temp}&#8457;</li>
@@ -39,49 +39,49 @@ var getCurrentConditions = (event) => {
             </ul>`;
             $('#current-weather').html(currentWeatherHTML);
             let latitude = response.coord.lat;
-        let longitude = response.coord.lon;
-        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
-        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
-        fetch(uvQueryURL)
-        .then(handleErrors)
-        .then((response) => {
-            return response.json();
+            let longitude = response.coord.lon;
+            let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+            uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+            fetch(uvQueryURL)
+                .then(handleErrors)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((response) => {
+                    let uvIndex = response.value;
+                    $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+                    if (uvIndex >= 0 && uvIndex < 3) {
+                        $('#uvVal').attr("class", "uv-favorable");
+                    } else if (uvIndex >= 3 && uvIndex < 8) {
+                        $('#uvVal').attr("class", "uv-moderate");
+                    } else if (uvIndex >= 8) {
+                        $('#uvVal').attr("class", "uv-severe");
+                    }
+                });
         })
-        .then((response) => {
-            let uvIndex = response.value;
-            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
-            if (uvIndex>=0 && uvIndex<3){
-                $('#uvVal').attr("class", "uv-favorable");
-            } else if (uvIndex>=3 && uvIndex<8){
-                $('#uvVal').attr("class", "uv-moderate");
-            } else if (uvIndex>=8){
-                $('#uvVal').attr("class", "uv-severe");
-            }
-        });
-    })
 }
 
 var getFiveDayForecast = (event) => {
     let city = $('#search-city').val();
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
     fetch(queryURL)
-    .then (handleErrors)
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        let fiveDayForecastHTML = `
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let fiveDayForecastHTML = `
         <h2>5-Day Forecast:</h2>
         <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
-        for (let i = 0; i < response.list.length; i++) {
-            let dayData = response.list[i];
-            let dayTimeUTC = dayData.dt;
-            let timeZoneOffset = response.city.timezone;
-            let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
-            let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
-            let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
-            if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
-                fiveDayForecastHTML += `
+            for (let i = 0; i < response.list.length; i++) {
+                let dayData = response.list[i];
+                let dayTimeUTC = dayData.dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    fiveDayForecastHTML += `
                 <div class="weather-card card m-2 p0">
                 <ul class="list-unstyled p-3">
                     <li>${thisMoment.format("MM/DD/YY")}</li>
@@ -91,11 +91,11 @@ var getFiveDayForecast = (event) => {
                     <li>Humidity: ${dayData.main.humidity}%</li>
                 </ul>
             </div>`;
-        }
-    }
-    fiveDayForecastHTML += `</div>`;
-    $('#five-day-forecast').html(fiveDayForecastHTML);
-})
+                }
+            }
+            fiveDayForecastHTML += `</div>`;
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        })
 }
 var saveCity = (newCity) => {
     let cityExists = false;
@@ -112,34 +112,52 @@ var saveCity = (newCity) => {
 }
 var renderCities = () => {
     $('#city-results').empty();
-    if (localStorage.length===0){
-        if (lastCity){
+    if (localStorage.length === 0) {
+        if (lastCity) {
             $('#search-city').attr("value", lastCity);
         } else {
             $('#search-city').attr("value", "Cooper City");
         }
-    } 
-    let lastCityKey="cities"+(localStorage.length-1);
-        lastCity=localStorage.getItem(lastCityKey);
+    } else {
+        let lastCityKey = "cities" + (localStorage.length - 1);
+        lastCity = localStorage.getItem(lastCityKey);
         $('#search-city').attr("value", lastCity);
         for (let i = 0; i < localStorage.length; i++) {
             let city = localStorage.getItem("cities" + i);
             let cityEl;
-            if (currentCity===""){
-                currentCity=lastCity;
+            if (currentCity === "") {
+                currentCity = lastCity;
             }
             if (city === currentCity) {
                 cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
             } else {
                 cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
-            } 
+            }
             $('#city-results').prepend(cityEl);
         }
-        if (localStorage.length>0){
+        if (localStorage.length > 0) {
             $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
         } else {
             $('#clear-storage').html('');
         }
     }
-    
+
 }
+
+$('#search-button').on("click", (event) => {
+    event.preventDefault();
+    currentCity = $('#search-city').val();
+    getCurrentConditions(event);
+});
+$('#city-results').on("click", (event) => {
+    event.preventDefault();
+    $('#search-city').val(event.target.textContent);
+    currentCity = $('#search-city').val();
+    getCurrentConditions(event);
+});
+$("#clear-storage").on("click", (event) => {
+    localStorage.clear();
+    renderCities();
+});
+renderCities();
+getCurrentConditions();
